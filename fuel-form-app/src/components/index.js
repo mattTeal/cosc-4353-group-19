8b1691236014e8bd0bb08e2353fbaa6b54450e2b
Matthew Teal <89936190+tattmeal@users.noreply.github.com>
2022-02-07 17:42:54 -0600

    Intialized frontend components in fuel-form-app/src/components/Pages. The file index.js in /Pages centralizes the export of each component into one file.

---
+++ fuel-form-app/src/components/index.js
@@ -0,0 +1,11 @@
+import LoginPage from './LoginPage/LoginPage'
+import FuelFormPage from './FuelFormPage/FuelFormPage'
+import ProfilePage from './ProfilePage/ProfilePage'
+import RegisterPage from './RegisterPage/RegisterPage'
+
+export {
+    LoginPage,
+    FuelFormPage,
+    ProfilePage,
+    RegisterPage
+}
