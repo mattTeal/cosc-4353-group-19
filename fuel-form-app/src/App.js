c63366f17787da49065234a915ecabfcc72a7f9b
Matthew Teal <89936190+tattmeal@users.noreply.github.com>
2022-02-07 18:02:59 -0600

    Initialized router to navigate in between frontend components. Also initialized skeleton code for components to verify functionality of React Router.

--- fuel-form-app/src/App.js
+++ fuel-form-app/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { LoginPage, ProfilePage, RegisterPage, FuelFormPage } from './components';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={FuelFormPage}/>
+        <Route exact path='/register' element={RegisterPage}/>
+        <Route exact path='/login' element={LoginPage}/>
+        <Route exact path='/profile' element={ProfilePage}/>
+      </Routes>
+    </Router>
   );
 }
 
