d26796fe0e97b6f7bbd594c19b853be407dc1367
Matthew Teal <89936190+tattmeal@users.noreply.github.com>
2022-02-07 18:11:51 -0600

    Code will successfully compile. Fixed mispellings in prior branch commits. Also implemented index.html in /fuel-form-app/public because it is required for app compilation. It was deleted upon app intialization.

--- fuel-form-app/src/App.js
+++ fuel-form-app/src/App.js
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import { LoginPage, ProfilePage, RegisterPage, FuelFormPage } from './components';
+import { LoginPage, ProfilePage, RegisterPage, FuelFormPage } from './components/Pages';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path='/' element={FuelFormPage}/>
-        <Route exact path='/register' element={RegisterPage}/>
-        <Route exact path='/login' element={LoginPage}/>
-        <Route exact path='/profile' element={ProfilePage}/>
+        <Route exact path='/' element={<FuelFormPage/>}/>
+        <Route exact path='/register' element={<RegisterPage/>}/>
+        <Route exact path='/login' element={<LoginPage/>}/>
+        <Route exact path='/profile' element={<ProfilePage/>}/>
       </Routes>
     </Router>
   );
