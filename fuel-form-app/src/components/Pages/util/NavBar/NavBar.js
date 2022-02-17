3baf5895a791d989c40b84a3260f327e240da236
akrepel <78815502+akrepel@users.noreply.github.com>
2022-02-17 14:31:11 -0600

    Update NavBar.js

--- fuel-form-app/src/components/Pages/util/NavBar/NavBar.js
+++ fuel-form-app/src/components/Pages/util/NavBar/NavBar.js
@@ -7,16 +7,33 @@ const NavBar = () => {
         <div className="topnav">
             <div className="container">
                 <div className="left-nav">
-                        <a href="/" className="compName"><span>Fi</span>eco</a>
+                        <a href="/fuel" className="compName"><span>Fi</span>eco</a>
                     </div>
                 <nav>  
                     <div className="right-nav">
                         <ul>
+<<<<<<< Updated upstream
                             <li><a href="/profile">Profile</a></li>
                             <li><a href="/">Sign Out</a></li>
                             
                             {/* <li><a href="/login">Login</a></li> */}
                             {/* <li><a href="/register">Register</a></li> */}
+=======
+<<<<<<< Updated upstream
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/profile">Profile</a></li>
+                            <li><a href="/login">Login</a></li>
+                            <li><a href="/register">Register</a></li>
+=======
+                            <li><a href="/fuel">Fuel Quote</a></li>
+                            <li><a href="/profile">Profile</a></li>
+                            <li><a href="/">Sign Out</a></li>
+                            
+                            
+                            {/* <li><a href="/login">Login</a></li> */}
+                            {/* <li><a href="/register">Register</a></li> */}
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
                         </ul>
                     </div>
                 </nav>
