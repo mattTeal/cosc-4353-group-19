b573628cf59c69bd3dfc00192de78b7f869b8c8d
Matthew Teal <89936190+tattmeal@users.noreply.github.com>
2022-02-19 21:36:40 -0600

    Updated NavBar.js

--- fuel-form-app/src/components/Pages/util/NavBar/NavBar.js
+++ fuel-form-app/src/components/Pages/util/NavBar/NavBar.js
@@ -11,10 +11,8 @@ const NavBar = () => {
                 <nav>  
                     <div className="right-nav">
                         <ul>
-                            <li><a href="/">Home</a></li>
                             <li><a href="/profile">Profile</a></li>
-                            <li><a href="/login">Login</a></li>
-                            <li><a href="/register">Register</a></li>
+                            <li><a href="/login">Sign Out</a></li>
                         </ul>
                     </div>
                 </nav>
