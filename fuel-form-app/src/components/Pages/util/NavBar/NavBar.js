fb90976d3f54f861ab7f51819c96628e6587c2e9
Matthew Teal <89936190+tattmeal@users.noreply.github.com>
2022-02-19 21:29:29 -0600

    Updated navbar.

--- fuel-form-app/src/components/Pages/util/NavBar/NavBar.js
+++ fuel-form-app/src/components/Pages/util/NavBar/NavBar.js
@@ -6,10 +6,18 @@ const NavBar = () => {
         <div className="topnav">
             <div className="container">
                 <div className="left-nav">
-                    <a href="/fuel" className="compName"><span>Fi</span>eco</a>
-                    <a href="/profile">Profile</a>
-                    <a href="/">Sign Out</a>
-                </div>
+                        <a href="/" className="compName"><span>Fi</span>eco</a>
+                    </div>
+                <nav>  
+                    <div className="right-nav">
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/profile">Profile</a></li>
+                            <li><a href="/login">Login</a></li>
+                            <li><a href="/register">Register</a></li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
         </div>
     );
