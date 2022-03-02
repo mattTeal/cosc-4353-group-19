99da0883e7e38320dac5bdb03e044b159aa2857b
vlzambr2 <vanlouisezam@gmail.com>
2022-03-02 17:17:00 -0600

    backend initial setup

---
+++ fuel-form-app/backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+const router = require("./routes");
+
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8080;
+const baseDir = process.cwd();
+
+app.use("/api", router);
+app.use(express.static(path.join(baseDir, 'build'), {
+    extensions: ['html', 'htm']
+}));
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+  console.log(path.join(baseDir, 'build'))
+});
