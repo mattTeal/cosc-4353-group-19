da6d2cd19f6fc0c1340b40ed29dc1b40e816750f
Matthew Teal <89936190+tattmeal@users.noreply.github.com>
2022-03-07 19:00:28 -0600

    Implemented backend code for quotes.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
-const router = require("./routes");
-
+const routes = require("./routes/Quotes");
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
 const baseDir = process.cwd();
 
-app.use("/api", router);
+app.use(express.json());
+app.use("/quotes", routes)
+
+app.get("/", (req, res) => {
+    res.send("hello root")
+});
 // app.use(express.static(path.join(baseDir, 'build'), {
 //     extensions: ['html', 'htm']
 // }));
