9b0cfc49082487f0ab9ec14e7ec24ba46e656e20
vlzambr2 <vanlouisezam@gmail.com>
2022-03-08 13:57:42 -0600

    profileRputer declared

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const dotenv = require("dotenv");
 const path = require("path");
 const router = require("./routes");
 
-
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
