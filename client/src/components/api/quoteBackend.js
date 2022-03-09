e0b6feefc4203668c50e42ce2c17712cba4d636a
Matthew Teal <89936190+tattmeal@users.noreply.github.com>
2022-03-09 17:38:22 -0600

    implemented frontend backend interaction.

---
+++ client/src/components/api/quoteBackend.js
@@ -0,0 +1,30 @@
+const BASE_ENDPOINT = "https://www.localhost:8080"
+const HEADER = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json;charset=UTF-8'
+}
+
+export const getQuotes = () => {
+    return (
+        fetch(
+            `${BASE_ENDPOINT}/quotes`
+        ).then(
+            result => result.ok? result.json() : {error: true}
+        )
+    )
+}
+
+export const createQuote = (data) => {
+    return (
+        fetch(
+            `${BASE_ENDPOINT}/quotes`,
+            {
+                method: "POST",
+                headers: HEADER,
+                body: JSON.stringify(data)
+            }
+        ).then(
+            result => result.ok ? result.json() : {error: true}
+        )
+    )
+}
