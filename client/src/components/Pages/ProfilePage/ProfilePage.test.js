d3a20bf4008b7f5a8f218a319086115bf85be368
Matthew Teal <89936190+tattmeal@users.noreply.github.com>
2022-03-13 17:08:19 -0500

    getUser test function passes, todo: test edit

---
+++ client/src/components/Pages/ProfilePage/ProfilePage.test.js
@@ -0,0 +1,65 @@
+const BASE_ENDPOINT = "https://localhost:8080/api";
+const HEADER = {
+    'Accept': 'application/json',
+    'Content-type': 'application/json;charset=utf-8',
+}
+
+afterEach(() => {
+    global.fetch.mockClear();
+    delete global.fetch;
+})
+
+test('get user', () => {
+    global.fetch = jest.fn(() =>
+        Promise.resolve({
+            ok: true,
+            json: () => Promise.resolve({
+                user: [{name: 'MockUser'}]
+            })
+        })
+    )
+
+    return (
+        fetch(`${BASE_ENDPOINT}/profile`, {
+            method:"GET",
+            redirect:"follow",
+        }).then(
+            response => response.json()
+        ).then(result => {
+            expect(global.fetch).toHaveBeenCalledWith(
+                `${BASE_ENDPOINT}/profile`,
+                {"method": "GET", "redirect": "follow"}
+            )
+            expect(result).toEqual({user: [{name: 'MockUser'}]})
+        })
+    )
+})
+
+test('edit profile test', () => {
+    const mockData = {firstName: "firstName"}
+    global.fetch = jest.fn().mockReturnValue(Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({
+            quotes: [mockData]
+        })
+    }))
+
+    return (
+        fetch(`${BASE_ENDPOINT}/profile`,
+            {
+                method: 'POST',
+                header: HEADER,
+                body: JSON.stringify(mockData)
+            }
+        ).then(result => {
+            expect(global.fetch).toHaveBeenCalledWith(
+                `${BASE_ENDPOINT}/profile`,
+                {
+                    method: "POST",
+                    headers: HEADER,
+                    body: JSON.stringify(mockData)
+                }
+            )
+        })
+    )
+})
