9a3711a4d95800f03d1c6fec227d521a247669f7
Matthew Teal <89936190+tattmeal@users.noreply.github.com>
2022-03-13 18:12:08 -0500

    testing login page implemented.

---
+++ client/src/components/Pages/LoginPage/LoginPage.test.js
@@ -0,0 +1,43 @@
+const BASE_ENDPOINT = "https://localhost:8080/api";
+const HEADER = {
+    'Accept': 'application/json',
+    'Content-type': 'application/json;charset=utf-8',
+}
+
+afterEach(() => {
+    global.fetch.mockClear();
+    delete global.fetch;
+})
+
+test('test login post', () => {
+    const mockData = {username: "username", password: "password"}
+    global.fetch = jest.fn().mockReturnValue(Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({
+            login: [mockData]
+        })
+    }))
+
+    return (
+        fetch(
+            `${BASE_ENDPOINT}/login`,
+            {
+                method: 'POST',
+                header: HEADER,
+                body: JSON.stringify(mockData)
+            }
+        ).then(
+            response => response.json()
+        ).then(result => {
+            expect(global.fetch).toHaveBeenCalledWith(
+                `${BASE_ENDPOINT}/login`,
+                {
+                    method: 'POST',
+                    header: HEADER,
+                    body: JSON.stringify(mockData)
+                }
+            )
+            expect(result).toEqual({login: [mockData]})
+        })
+    )
+})
