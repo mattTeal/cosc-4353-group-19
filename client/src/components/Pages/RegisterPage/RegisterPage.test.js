580ca30a69e260002bbac81a18b8dd2a849d08d5
Matthew Teal <89936190+tattmeal@users.noreply.github.com>
2022-03-13 21:06:06 -0500

    Wrote test for register page.

--- client/src/components/Pages/RegisterPage/RegisterPage.test.js
+++ client/src/components/Pages/RegisterPage/RegisterPage.test.js
@@ -8,3 +8,36 @@ afterEach(() => {
     global.fetch.mockClear();
     delete global.fetch;
 })
+
+test('test register post', () => {
+    const mockData = {username: "username", password: "password", confirmpassword: "password"}
+    global.fetch = jest.fn().mockReturnValue(Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({
+            register: [mockData]
+        })
+    }))
+
+    return (
+        fetch(
+            `${BASE_ENDPOINT}/register`,
+            {
+                method: 'POST',
+                header: HEADER,
+                body: JSON.stringify(mockData)
+            }
+        ).then(
+            response => response.json()
+        ).then(result => {
+            expect(global.fetch).toHaveBeenCalledWith(
+                `${BASE_ENDPOINT}/register`,
+                {
+                    method: 'POST',
+                    header: HEADER,
+                    body: JSON.stringify(mockData)
+                }
+            )
+            expect(result).toEqual({register: [mockData]})
+        })
+    )
+})
