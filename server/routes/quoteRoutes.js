81b82b253c41ac54f85ee2e89a95d3ebb7adbaab
Matthew Teal <89936190+tattmeal@users.noreply.github.com>
2022-03-19 19:50:19 -0500

    posts now update database.

--- server/routes/quoteRoutes.js
+++ server/routes/quoteRoutes.js
@@ -7,14 +7,11 @@ router.get('/', (req, res) => {
     res.status(200).send(mockDB.quoteHistory);
 })
 
-router.post('/quotes', (req, res) => {
-    const quoteData = {
-        gallons: req.body.gallons.trim() || "",
-        date: req.body.date || "",
-    }
-    console.log(quoteData);
+router.post('/', (req, res) => {
+    const {gallons, date} = req.body;
+    console.log(gallons, date);
     try {
-        db.promise().query(`INSERT INTO QUOTES VALUES('${gallons}', '${date}'`)
+        db.promise().query(`INSERT INTO QUOTES VALUES('${gallons}', '${date}')`)
         res.status(201).send("Post completed");    
     } catch (error) {
         console.log(error)
